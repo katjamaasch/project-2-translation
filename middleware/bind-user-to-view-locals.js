eff9256d6524e02df1817c94afceb17a2f167cec
Sok Mun Wunder <sokmunwunder@gmail.com>
2021-02-11 21:22:58 +0100

    Codes by Ze on viewer/publisher

--- middleware/bind-user-to-view-locals.js
+++ middleware/bind-user-to-view-locals.js
@@ -2,5 +2,9 @@
 
 module.exports = (req, res, next) => {
   res.locals.user = req.user;
+  if (req.user) {
+    res.locals.userIsPublisher = req.user.role === 'publisher';
+    res.locals.userIsViewer = req.user.role === 'viewer';
+  }
   next();
 };
